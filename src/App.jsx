6234239f848736fd75b1b968d7718199e46e7c06
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2025-01-05 22:41:59 +0700

    init project awards winning website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero';
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
+    </main>
+  )
+}
+
+export default App
