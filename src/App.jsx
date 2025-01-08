93b88d92a47d14f184ade4f201824ab1fa87c0ed
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2025-01-08 23:56:56 +0700

    implement about section and navbar and animatedTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import About from './components/About';
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
       <Hero />
-
-      <section className='z-0 min-h-screen bg-blue-500'/>
+      <About />
     </main>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// https://youtu.be/zA9r5zTllx4?t=6044
+
+const Features = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default Features
